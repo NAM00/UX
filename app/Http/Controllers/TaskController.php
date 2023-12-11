076706c8f23d2643fd169897961e6dacdcd64f8d
NAM00 <66006043+NAM00@users.noreply.github.com>
2023-12-11 06:27:21 +0600

    update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -26,6 +26,8 @@ class TaskController extends Controller
         {
             Session::put('CAMP_ID', $CAMP_ID);
             Session::put('MWId', $MWId);
+            $worker_id=Session::get('MWId');
+            echo $worker_id;
 
             if(Task::count() != null)
             {
